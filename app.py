b686a089366e41e3d084a62287df4f50c9d717b4
Vờyҝ <denistroy04@gmail.com>
2025-06-04 14:40:20 +0500

    d
    dd

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def hitmo_search():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
